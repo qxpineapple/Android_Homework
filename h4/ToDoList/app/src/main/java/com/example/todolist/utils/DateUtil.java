fd3c193e20a591338eec246ad06816d6a8b85ec6
qxpineapple <1312661951@qq.com>
2021-11-09 10:34:15 +0800

    homework4

---
+++ h4/ToDoList/app/src/main/java/com/example/todolist/utils/DateUtil.java
@@ -0,0 +1,39 @@
+package com.example.todolist.utils;
+
+import android.content.res.Resources;
+import android.os.TokenWatcher;
+import android.text.format.DateUtils;
+
+import com.example.todolist.R;
+import com.example.todolist.activity.MyApplication;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtil {
+    private static Resources resources= MyApplication.getContext().getResources();
+    private static Calendar calendar=Calendar.getInstance();
+    public static String getYearMonthDayNumberic(Date date){
+        calendar.setTime(date);
+        int year=calendar.get(Calendar.YEAR);
+        int month=calendar.get(Calendar.MONTH)+1;
+        int day=calendar.get(Calendar.DAY_OF_MONTH);
+        return String.format(resources.getString(R.string.year_month_day_numberic),year,month,day);
+    }
+    public static String getYearAndMonth(Date date){
+        calendar.setTime(date);
+        int year=calendar.get(Calendar.YEAR);
+        int month=calendar.get(Calendar.MONTH)+1;
+        return String.format(resources.getString(R.string.year_and_month),year,month);
+    }
+    public static String getYearMonthDay(Date date){
+        calendar.setTime(date);
+        int year=calendar.get(Calendar.YEAR);
+        int month=calendar.get(Calendar.MONTH)+1;
+        int day=calendar.get(Calendar.DAY_OF_MONTH);
+        return String.format(resources.getString(R.string.year_month_day),year,month,day);
+    }
+
+}
